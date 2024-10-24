82a8e37da8115e445e9668d8c6b721d6d769e99f
dganesan <dganesan@smythstoys.com>
2024-10-24 15:12:12 +0530

    imported style.css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'src/styles.css'
+
+
 // src/index.js
 export { default as FirstNameInput } from './components/FirstNameInput';
 export { default as LastNameInput } from './components/LastNameInput';
