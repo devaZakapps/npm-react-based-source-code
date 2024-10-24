83e2ec3a92388e09ca838bf90eee24c33951d978
dganesan <dganesan@smythstoys.com>
2024-10-24 14:16:43 +0530

    Added tailwind css configuration

---
+++ src/MyComponent.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import './css/global.css';  // Adjust path as necessary
+
+const MyComponent = () => {
+    return (
+        <div className="p-4 bg-blue-500 text-white rounded">
+            This is a Tailwind-styled component.
+        </div>
+    )
+}
+
+
+export default MyComponent
